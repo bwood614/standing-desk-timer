808798b2d78c131e45b25ceb551f27a1c752ca2c
Brayden Wood <braydenwood1@gmail.com>
2024-09-10 18:53:18 -0600

    remove old files

--- background.js
+++
@@ -1,3 +0,0 @@
-// Extensions can monitor browser events in the background using the extension's service
-// worker. Service workers are special JavaScript environments that handle events and
-// terminate when they're not needed.
